3fea3585fc5362f89c25fbc8e1fd53f0e4c35986
Thomasrbm <thomas.robert76@hotmail.com>
2025-11-23 05:45:06 +0100

    Well advanced

--- srcs/requirements/nginx/tools/generate_cert.sh
+++ srcs/requirements/nginx/tools/generate_cert.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+
 openssl req -x509 -nodes -newkey rsa:2048 \
+  -subj "/CN=throbert.42.fr" \
   -keyout /etc/nginx/ssl/server.key \
-  -out /etc/nginx/ssl/server.crt \
   -days 365 \
-  -subj "/CN=${DOMAIN_NAME}"
+  -out /etc/nginx/ssl/server.crt
+
